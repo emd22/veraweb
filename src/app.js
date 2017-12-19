e79436f4d0f4ca5e525ce3687284405034dd6cbf
= <ethanmacdonald04@gmail.com>
2017-12-19 15:43:57 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Draggable, Droppable} from 'react-drag-and-drop';
+import './app.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
